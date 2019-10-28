325dc226587464d9569cca1ad2e71f18313a61af
Infocoms <kevincomyn@gmail.com>
2019-10-29 00:23:14 +0100

    made list of pokemon dsiplaying img and name

--- index.php
+++ index.php
@@ -5,79 +5,26 @@
 
 
 //FETCH THE API JSON FILE for the given ID or name
+
 for ($i = 1; $i <= 20; $i++){
 $url = "https://pokeapi.co/api/v2/pokemon/$i";
-$id = $_POST['inpput'];
 $data = file_get_contents($url);
 $pokemon = json_decode($data);
 
 
     $datatemplate = array (
-        $pokemon->id,
-        $pokemon->sprites->front_default,
-        $pokemon->name,
 
+        '<div class="container">
+        <div class="pokedex">
+    
+    <div class="card mb-4">'.$id = $pokemon->id,
+        '<img class="card-image" src="'.$pokemon->sprites->front_default.'"/>',
+        '<div class="card-body"><h2 class="card-title">'.$pname = $pokemon->name.'</h2>',
+        '<p class="card-subtitle">Type:'.$ptype = $pokemon->types[0]->type->name.'</p>'
+            .'</div></div></div></div>'
     );
 
-
-    var_dump($datatemplate);
-
-
-}
-
-
-
-
-
-
-
-
-
-//Fetch 4 random moves from moves array
-$rand_keys = array_rand($pokemon->moves, 4);
-$move1 = $pokemon->moves[$rand_keys[0]]->move->name;
-$move2 = $pokemon->moves[$rand_keys[1]]->move->name;
-$move3 = $pokemon->moves[$rand_keys[2]]->move->name;
-$move4 = $pokemon->moves[$rand_keys[3]]->move->name;
-
-
-//Image for the main pokemon the type and the evolution link
-
-$type = $pokemon->types[0]->type->name;
-$evolink = $pokemon->species->url;
-
-
-
-
-
-//Get the content of the evolution pokemon name and evolves from LINK
-$evodata = file_get_contents($evolink);
-$evolution = json_decode($evodata);
-$evopokname = $evolution->evolves_from_species->name;
-$evopokurl = $evolution->evolves_from_species->url;
-
-
-//Evolves from link data being fetched and decoded into php while also fetching the url of the pokemon for later use
-$imgevo = file_get_contents($evopokurl);
-$evoimg = json_decode($imgevo);
-$evoimglink = $evoimg->varieties[0]->pokemon->url;
-
-
-//the url from before (LATER USE) is being used to fetch the image of the previous pokemon
-$evoimgfetch = file_get_contents($evoimglink);
-$evoimgdata = json_decode($evoimgfetch);
-$evoimglinkfinal = $evoimgdata->sprites->front_default;
-
-
-//if no prev evolution image then show default pokeball
-if ($evoimglinkfinal === NULL) {
-    $evoimglinkfinal = "Img/pokeball.svg";
-}
-
-
-//if no prev evolution poké name then show NO PREV EVOLUTION
-if ($evopokname === NULL) {
-    $evopokname = "No Prev Evolution";
+    echo implode($datatemplate);
 }
 ?>
 
@@ -92,15 +39,25 @@ if ($evopokname === NULL) {
 
     <title>Pokedex</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></head>
 <body>
 
-
-<div id="Pokedex">
+<div class="container">
+    <div class="pokedex">
+    </div>
 </div>
 
-<form id="searchBox" method="post">
+
+
+
+
+
+
+<!--<form id="searchBox" method="post">
     <label>
         <input type="text" name="inpput" class="form-control" id="inpput" value="" placeholder="Poké Name">
     </label>
@@ -112,29 +69,29 @@ if ($evopokname === NULL) {
 <div id="infoBox">
     <p id="name" value="">
         <?php
-        echo "Name: &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp ID: $pokemon->id <br> Type: $type"; ?>
+/*        echo "Name: &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp ID: $pokemon->id <br> Type: $type"; */?>
     </p>
     <div class="overflow-auto" id="move"><?php
-        echo "Moves: <br> $move1 <br> $move2 <br> $move3 <br> $move4 <br>"; ?> </div>
+/*        echo "Moves: <br> $move1 <br> $move2 <br> $move3 <br> $move4 <br>"; */?> </div>
 
 </div>
 <div id="displayBoxBg">
     <p id="prevEvo" data-placeholder="test"><?php
-        echo "$evopokname"; ?></p>
+/*        echo "$evopokname"; */?></p>
     <div id="displayBox2">
         <img id="prevEvoImg" src="<?php
-        echo "$evoimglinkfinal"; ?>" alt="no prev evolution">
+/*        echo "$evoimglinkfinal"; */?>" alt="no prev evolution">
     </div>
 </div>
 
 
 <form id="searchBox2" method="post">
-    <button id="left" type="submit" value="<?php echo $id = $_POST['inpput']-1; ?>" name="inpput" ><</button>
+    <button id="left" type="submit" value="<?php /*echo $id = $_POST['inpput']-1; */?>" name="inpput" ><</button>
 </form>
 
 <form id="searchBox3" method="post">
-<button id="right" type="submit" value="<?php echo $id = $_POST['inpput']+1; ?>" name="inpput"> ></button>
-</form>
+<button id="right" type="submit" value="<?php /*echo $id = $_POST['inpput']+1; */?>" name="inpput"> ></button>
+</form>-->
 
 </body>
 

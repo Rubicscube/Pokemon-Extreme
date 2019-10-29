08653e5a2062c1914020223f1ea3e3bd76b74723
infocoms <kevincomyn@gmail.com>
2019-10-29 11:04:45 +0100

    fetched around 150 pokemons and made them clickable to redirect them to their information

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 
 //FETCH THE API JSON FILE for the given ID or name
 
-for ($i = 1; $i <= 20; $i++){
+
+for ($i = 1; $i <= 150; $i++){
 $url = "https://pokeapi.co/api/v2/pokemon/$i";
 $data = file_get_contents($url);
 $pokemon = json_decode($data);
@@ -15,13 +16,12 @@ $pokemon = json_decode($data);
     $datatemplate = array (
 
         '<div class="container">
-        <div class="pokedex">
-    
+        <div class="pokedex">'.'<a href="https://pokeapi.co/api/v2/pokemon/'.$pokemon->id.'">
     <div class="card mb-4">'.$id = $pokemon->id,
         '<img class="card-image" src="'.$pokemon->sprites->front_default.'"/>',
         '<div class="card-body"><h2 class="card-title">'.$pname = $pokemon->name.'</h2>',
         '<p class="card-subtitle">Type:'.$ptype = $pokemon->types[0]->type->name.'</p>'
-            .'</div></div></div></div>'
+            .'</a></div></div></div></div>'
     );
 
     echo implode($datatemplate);
@@ -48,50 +48,12 @@ $pokemon = json_decode($data);
 
 <div class="container">
     <div class="pokedex">
+
+
+
+
     </div>
 </div>
-
-
-
-
-
-
-
-<!--<form id="searchBox" method="post">
-    <label>
-        <input type="text" name="inpput" class="form-control" id="inpput" value="" placeholder="Poké Name">
-    </label>
-    <button id="search" type="submit"><i class="material-icons">search</i></button>
-</form>
-
-
-
-<div id="infoBox">
-    <p id="name" value="">
-        <?php
-/*        echo "Name: &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp ID: $pokemon->id <br> Type: $type"; */?>
-    </p>
-    <div class="overflow-auto" id="move"><?php
-/*        echo "Moves: <br> $move1 <br> $move2 <br> $move3 <br> $move4 <br>"; */?> </div>
-
-</div>
-<div id="displayBoxBg">
-    <p id="prevEvo" data-placeholder="test"><?php
-/*        echo "$evopokname"; */?></p>
-    <div id="displayBox2">
-        <img id="prevEvoImg" src="<?php
-/*        echo "$evoimglinkfinal"; */?>" alt="no prev evolution">
-    </div>
-</div>
-
-
-<form id="searchBox2" method="post">
-    <button id="left" type="submit" value="<?php /*echo $id = $_POST['inpput']-1; */?>" name="inpput" ><</button>
-</form>
-
-<form id="searchBox3" method="post">
-<button id="right" type="submit" value="<?php /*echo $id = $_POST['inpput']+1; */?>" name="inpput"> ></button>
-</form>-->
 
 </body>
 

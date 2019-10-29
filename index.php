7b9be278ab902cf015c100d7ba75372b58276480
infocoms <kevincomyn@gmail.com>
2019-10-29 17:08:10 +0100

    changed background for cards and added a font for the text

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Pokedex</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
+</head>
+<body>
 <div class="container">
     <div class="pokedex">
         <form method="post">
@@ -20,7 +40,7 @@
                         <option value="40">40</option>
                         <option value="50">50</option>
                         <option value="100">100</option>
-                        <option value="807">807 takes 3 mins to load</option>
+                        <option value="807">807 takes 2 mins to load</option>
                     </select>
                 </div>
                 <button class="btn btn-primary" type="submit" name="submit"> apply</button>
@@ -50,7 +70,7 @@ if (isset($_POST['dropdownvalue'])) {
         $datatemplate = array(
 
             '<div class="col-md-3"><a href="https://pokeapi.co/api/v2/pokemon/' . $pokemon->id . '">
-    <div class="card mb-4 bg-warning">' . $id = $pokemon->id,
+    <div class="card mb-4">' . $id = $pokemon->id,
             '<img class="card-image" src="' . $pokemon->sprites->front_default . '"/>',
             '<div class="card-body"><h2 class="card-title">' . $pname = $pokemon->name . '</h2>',
             '<p class="card-subtitle">Type: ' . $ptype = $pokemon->types[0]->type->name . '</p></a></div></div></div>'
@@ -66,26 +86,7 @@ if (isset($_POST['dropdownvalue'])) {
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <link rel="stylesheet" href="css/style.css">
 
-    <title>Pokedex</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
-<body>
 <nav aria-label="Page navigation example" class="pagination">
     <ul class="pagination">
         <li class="page-item"><a class="page-link" href="#">Previous</a></li>

5233e485e599a6dd0b8b3d32ecb87fe052867886
kevin comyn <kevincomyn@gmail.com>
2019-11-04 02:35:44 +0100

    added back to top button
    - back to top button on index.php

--- index.php
+++ index.php
@@ -160,6 +160,30 @@ echo '</div></div>';
         <li class="list-inline-item"><a href="https://pokeapi.co/">pokéApi</a></li>
         <li class="list-inline-item"><a href="https://github.com/Rubicscube/Pokemon-Extreme">Github</a></li>
     </ul>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 </footer>
+
+
+<script>
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
 </body>
 </html>
